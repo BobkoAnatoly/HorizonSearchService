b2a3512dcd7d9e7bbe39963f96f5f2c561fe163d
Tolya <tolya.bob@inbox.ru>
2022-10-12 19:09:56 +0300

    add connectionClient

---
+++ HorizonSearchPlatform.Server/ConnectionClient.cs
@@ -0,0 +1,69 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+namespace HorizonSearchPlatform.Server
+{
+    public class ConnectionClient
+    {
+        private TcpListener Listener { get; set; }
+        public ConnectionAddress ConnectionAddress { get; private set; }
+
+        public ConnectionClient(ConnectionAddress сonnectionАddress)
+        {
+            ConnectionAddress = сonnectionАddress;
+        }
+
+        public TcpListener GetListener()
+        {
+            Listener = new TcpListener(IPAddress.Parse(ConnectionAddress.Ip), ConnectionAddress.Port);
+            return Listener;
+        }
+
+        public void Process(object client)
+        {
+            NetworkStream stream = default;
+            var _tcpClient = client as TcpClient;
+            try
+            {
+                //Получение сетевого потока
+                stream = _tcpClient.GetStream();
+
+                byte[] buffer = new byte[_tcpClient.ReceiveBufferSize];
+
+                StringBuilder response = new StringBuilder();
+                int bytes = default;
+
+                do
+                {
+                    bytes = stream.Read(buffer, 0, buffer.Length);
+                    response.Append(Encoding.Unicode.GetString(buffer, 0, bytes));
+                }
+                while (stream.DataAvailable);
+
+                string json = response.ToString();
+
+                //var query = JsonSerializer.Deserialize<ServerQuery>(json)!;
+
+                //json = HandlerType.SearchType(query, _methodService);
+
+                buffer = Encoding.Unicode.GetBytes(json);
+                stream.Write(buffer, 0, buffer.Length);
+
+            }
+
+            catch (Exception ex) { Console.WriteLine("Error: " + ex.Message); }
+
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+
+                if (_tcpClient != null)
+                    _tcpClient.Close();
+            }
+        }
+    }
+}
---
+++ HorizonSearchPlatform.Server/Program.cs
@@ -0,0 +1,69 @@
+﻿using HorizonSearchPlatform.Server;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System.Net.Sockets;
+
+using IHost host = CreateHostBuilder(args);
+
+
+var configurationBuilder = new ConfigurationBuilder();
+BuildConfig(configurationBuilder);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configurationBuilder.Build())
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+Log.Logger.Information("Server starting");
+
+var configuration = host.Services.GetService<IConfiguration>();
+
+
+var address = configuration.GetSection("ConnectionInfo").Get<ConnectionAddress>();
+
+ConnectionClient connectionClient = new ConnectionClient(address);
+
+var listener = connectionClient.GetListener();
+
+try
+{
+    listener.Start();
+
+
+    while (true)
+    {
+        var client = listener.AcceptTcpClient();
+
+        var processingThread = new Thread(new ParameterizedThreadStart(connectionClient.Process));
+        processingThread.Start(client);
+    }
+}
+catch (Exception ex)
+{
+    Log.Error(ex.Message);
+}
+finally
+{
+    if (listener != null)
+        listener.Stop();
+}
+
+IHost CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+        .ConfigureServices((context, services) =>
+        {
+
+        })
+        .UseSerilog()
+        .Build();
+}
+static void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", false, true)
+        .AddEnvironmentVariables();
+
+}
